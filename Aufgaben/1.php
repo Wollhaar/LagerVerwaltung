df29bb85465c96eeef5335090fc6294143f74496
David Goraj <david_goraj@yahoo.de>
2021-03-23 15:59:26 +0100

    0.0.0.3 solved Aufgaben 1 + 2

--- Aufgaben/1.php
+++ Aufgaben/1.php
@@ -1,16 +1,19 @@
 <?php
 
-// Bitte mal prüfen ob Buchstabenanzahl gerade oder ungerade ist - es zählen sich nur kleine und große Buchstaben.
+// Bitte mal prüfen ob Buchstabenanzahl gerade oder ungerade ist -
+// es zählen sich nur kleine und große Buchstaben.
 
 function antwort($data) {
     $antwort = false;
-    // Hier schreibst Du Deine Lösung
+    $data = preg_replace('/([^A-Za-z].)*/', '', $data);
+    if (!is_float(strlen($data) / 2)) $antwort = true;
 
     return $antwort;
 }
 
-
 echo 'Test 1 ' . ($a = 'abcdefghij') . ' <br> soll: true, ist: ' . var_export(antwort($a), true) . '<br><br>';
 echo 'Test 2 ' . ($a = 'anskdTrht') . ' <br> soll: false, ist: ' . var_export(antwort($a), true) . '<br><br>';
-echo 'Test 3 ' . ($a = 'XNJI12MKO34nji56ka') . ' <br> soll: true, ist: ' . var_export(antwort($a), true) . '<br><br>';
+echo 'Test 3 ' . ($a = 'XNJI12MK034nji56ka') . ' <br> soll: false, ist: ' . var_export(antwort($a), true) . '<br><br>';
 echo 'Test 4 ' . ($a = '!"§$%&/()=?') . ' <br> soll: false, ist: ' . var_export(antwort($a), true) . '<br><br>';
+
+if (isset($_GET['match'])) echo '<br/>Output GET-Variable 2: ' . var_export(antwort($_GET['match']), true);
--- Aufgaben/2.php
+++ Aufgaben/2.php
@@ -1,16 +1,18 @@
 <?php
 
-// Funktion soll  aus gerade Stellen liefern
+// Funktion soll Zeichen aus gerade Stellen liefern
 
 function antwort($data) {
-    $antwort = false;
-    // Hier schreibst Du Deine Lösung
+    $antwort = '';
+    foreach (str_split($data, 2) as $value)
+        $antwort .= substr($value, 0, 1);
 
-    return $antwort;
+    return $antwort ?? false;
 }
 
+if (isset($_GET['_ijt'])) echo 'GET ' . $_GET['_ijt'] . ' <br> ' . var_export(antwort($_GET['_ijt']), true) . '<br><br>';
 
-echo 'Test 1 ' . ($a = 'abcdefghij') . ' <br> soll: acegi, ist: ' . var_export(antwort($a), true) . '<br><br>';
+echo 'Test 1 ' . ($a = 'abcdefghij') . ' <br> soll: acegj, ist: ' . var_export(antwort($a), true) . '<br><br>';
 echo 'Test 2 ' . ($a = 'anskdTrht') . ' <br> soll: asdrt, ist: ' . var_export(antwort($a), true) . '<br><br>';
 echo 'Test 3 ' . ($a = 'XNJI12MKO34nji56ka') . ' <br> soll: NI2K3ni6a, ist: ' . var_export(antwort($a), true) . '<br><br>';
 echo 'Test 4 ' . ($a = '!"§$%&/()=?') . ' <br> soll: "$&(=, ist: ' . var_export(antwort($a), true) . '<br><br>';
