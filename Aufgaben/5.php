adbc5e01c14bafd2d736bebeb514622253816887
David Goraj <david_goraj@yahoo.de>
2021-03-24 16:12:14 +0100

    0.0.0.6 Aufgabe 5 solved

--- Aufgaben/5.php
+++ Aufgaben/5.php
@@ -2,29 +2,48 @@
 
 // Der größte Wert in der Tabelle
 
-function antwort($data, $i = 0) {
+function antwort($data) {
+    $result = antwort1($data);
+
+    return max($result);
+}
+
+function antwort1($data) {
+
+    $neues = array();
     foreach ($data as $key => $value) {
-        if (is_array($value)) {
-            $max = antwort($value, $i++);
-            $antwort = $max;
-        }
-    elseif (is_int($value) && $max > $antwort) {
-            $max = max($data);
-        }
+        if (is_array($value))
+            $neues = array_merge($neues, antwort1($value));
+
+        else $neues[] = $value;
     }
-    echo '<pre>';
-    echo 'Array (Level: ' . $i . '): <br/>';
-    var_export($data);
-    echo 'antwort: <br/>';
-    var_export($antwort);
-    echo '</pre>';
 
-
-    return $antwort;
+    return $neues;
 }
 
 
-echo 'Test 1 Array: ' . implode(', ', $a = array(3,5,6,8,9,15,62,21,0,-16,33)) . ' <br> soll: 33, ist: ' . var_export(antwort($a), true) . '<br><br>';
-echo 'Test 2 Array: ' . implode(', ', $a = array(3,-3,-6,-8,-9,-15,-62,-21,-0,-16,-33)) . ' <br> soll: 3, ist: ' . var_export(antwort($a), true) . '<br><br>';
-echo 'Test 3 Array: ' . implode(', ', $a = array(1,5,9,array(16,22),array(18,21),20)) . ' <br> soll: 22, ist: ' . var_export(antwort($a), true) . '<br><br>';
-echo 'Test 4 Array: ' . implode(', ', $a = array(15,array(1,3,6,20,array(21,5,14,6)),array(array(array(17,13,array(60)),15),16),-10)) . ' <br> soll: 60, ist: ' . var_export(antwort($a), true) . '<br><br>';
+$a1 = array(3,5,6,8,9,15,62,21,0,-16,33);
+$a2 = array(3,-3,-6,-8,-9,-15,-62,-21,-0,-16,-33);
+
+$a3 = array(1,5,9,
+    array(16,22),
+    array(18,21),
+    20);
+
+$a4 = array(15,
+    array(1,3,6,20,
+        array(21,5,14,6)),
+    array(
+        array(
+            array(17,13,
+                array(60)
+            ),
+            15),
+        16),
+    -10);
+
+
+echo 'Test 1 Array: ' . json_encode($a1) . ' <br> soll: 33, ist: ' . var_export(antwort($a1), true) . '<br><br>';
+echo 'Test 2 Array: ' . json_encode($a2) . ' <br> soll: 3, ist: ' . var_export(antwort($a2), true) . '<br><br>';
+echo 'Test 3 Array: ' . json_encode($a3) . ' <br> soll: 22, ist: ' . var_export(antwort($a3), true) . '<br><br>';
+echo 'Test 4 Array: ' . json_encode($a4) . ' <br> soll: 60, ist: ' . var_export(antwort($a4), true) . '<br><br>';
