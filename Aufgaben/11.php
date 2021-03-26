fd2e023b4a7856c84e43720faac944e44189c50e
Charytoniuk <adrian.charytoniuk@maxpool.de>
2021-03-26 08:36:24 +0100

    Aufgabe 13

--- Aufgaben/11.php
+++ Aufgaben/11.php
@@ -3,15 +3,16 @@
 
 function antwort($data)
 {
+
 }
 
 
 echo 'Test 1 ' . ($a = array(array('a' => 10, 'b' => 5, 'c' => 20),
         array('a' => 10, 'b' => 5, 'c' => 20),
-        array('a' => 10, 'b' => 5, 'c' => 20))) . ' <br>, ist: ' . var_export(antwort($a), true) . '<br><br>';
+        array('a' => 10, 'b' => 5, 'c' => 20))) . ' <br><br>';
 echo 'Test 2 ' .  ($a = array(array('a' => 100, 'b' => 51, 'c' => 22),
         array('b' => 55, 'c' => 200),
-        array('a' => -10, 'b' => 57, 'c' => 20))) . ' <br>, ist: ' . var_export(antwort($a), true) . '<br><br>';
+        array('a' => -10, 'b' => 57, 'c' => 20))) . ' <br><br>';
 echo 'Test 3 ' . ($a = array(array('a' => -100, 'b' => 51, 'c' => 22),
         array('a' => 55, 'c' => 200),
-        array('a' => 77, 'b' => 57, 'c' => 20))) . ' <br>, ist: ' . var_export(antwort($a), true) . '<br><br>';
+        array('a' => 77, 'b' => 57, 'c' => 20))) . ' <br><br>';
---
+++ Aufgaben/13.php
@@ -0,0 +1,10 @@
+<?php
+// bitte Datei aus Aufgabe 11 lesen und eine summe aus Alle Ellemente Rechnen
+
+function antwort()
+{
+    return '';
+}
+
+
+echo 'Antwort: soll:  105, 495, 382<br>, ist: ' . var_export(antwort(), true) . '<br><br>';
