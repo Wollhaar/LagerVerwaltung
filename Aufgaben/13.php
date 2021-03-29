c751e7326b0954287991f1d89a40cea64e0f14c9
David Goraj <david_goraj@yahoo.de>
2021-03-29 10:14:29 +0200

    0.0.0.15 Aufgabe 13 in construction (wrong way)

--- Aufgaben/13.php
+++ Aufgaben/13.php
@@ -3,7 +3,26 @@
 
 function antwort()
 {
-    return '';
+    $path = $_SERVER['DOCUMENT_ROOT'] . '/storage/';
+
+    $antwort = array('a' => 0, 'b' => 0, 'c' => 0);
+    foreach (scandir($path) as $filename) {
+        if (!str_contains($filename, 'data')) continue;
+        if (file_exists($path . $filename)) {
+            $content = json_decode(file_get_contents($path . $filename), 'true');
+            echo '<h3>Content - values</h3><pre>';
+            print_r($content);
+            echo '</pre>';
+            foreach ($content as $values) {
+                $antwort['a'] += $values['a'];
+                $antwort['b'] += $values['b'];
+                $antwort['c'] += $values['c'];
+            }
+        }
+    }
+
+    $antwort = implode(', ', $antwort);
+    return $antwort;
 }
 
 
