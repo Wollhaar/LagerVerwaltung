fe66aeabbb6cbd0c3c6731793331c1c4861a33df
David Goraj <david_goraj@yahoo.de>
2021-04-12 11:16:11 +0200

    0.0.0.18 Aufgabe 16 solution (from Adrian)

--- Aufgaben/16.php
+++ Aufgaben/16.php
@@ -3,24 +3,24 @@
 
 function antwort($data)
 {
-    $unsorted = true;
-    while ($unsorted && ($data)) {
-        $i = 0;
-        foreach ($data as $key => $value) {
-            $value = $data[$key];
-            if (key_exists($key + 1, $data) && $value > $data[$key + 1]) {
-                array_push($data, $value);
-                unset($data[$key]);
-                $i++;
-            }
+    $data = array_values($data);
+    $i = 0;
+
+    while (isset($data[$i+1])) {
+        if ($data[$i]>$data[$i+1]) {
+            $temp = $data[$i];
+            $data[$i] = $data[$i+1];
+            $data[$i+1] = $temp;
+            if ($i > 0) $i--;
+        } else {
+            $i++;
         }
-        if ($i === 0) $unsorted = false;
-        $data = array_values($data);
     }
-    return $data;
+
+    return implode(',', $data);
 }
 
 
 echo 'Test 1 ' . json_encode($a = array(1, 6, 2, 5, 2, 6, 1, 2)) . ' <br> soll: 1,1,2,2,2,5,6,6, ist: ' . var_export(antwort($a), true) . '<br><br>';
-echo 'Test 2 ' . json_encode($a = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 8)) . ' <br> soll: 1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,8,9,9,10,10,11,11, ist: ' . var_export(antwort($a), true) . '<br><br>';
+echo 'Test 2 ' . json_encode($a = array(2, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 8)) . ' <br> soll: 1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,8,9,9,10,10,11,11, ist: ' . var_export(antwort($a), true) . '<br><br>';
 echo 'Test 3 ' . json_encode($a = array(1,1,1,1,2,3,5,4,1,-1,-2,-2,-2,2)) . ' <br> soll: -2,-2,-2,-1,1,1,1,1,1,2,2,3,4,5, ist: ' . var_export(antwort($a), true) . '<br><br>';
