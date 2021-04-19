4b8d6c3aac3dfd35b3385288519690a769628ea6
David Goraj <david_goraj@yahoo.de>
2021-04-19 18:58:22 +0200

    0.0.2 added library to project and modified gitignore, added some style

--- page/pub/js/main.js
+++ page/pub/js/main.js
@@ -1,11 +1,27 @@
 global_data = null;
 default_page = 'home';
 
+function init() {
+    default_link();
+}
+
+function default_link() {
+    $("a#header-link").attr('href', default_page);
+}
+
 function modal(header, content) {
     let modal = $(".modal").clone();
-    $(modal).find(".modal-header").text(header);
-    $(modal).find(".modal-body").text(content);
+    $(modal).find(".modal-header h5").text(header);
+    $(modal).find(".modal-body p").text(content);
 
     $("body").append(modal);
     $(modal).show();
 }
+
+$(document).ready(function() {
+    init();
+
+    $(".modal button:contains(Close)").click(function() {
+
+    });
+});
